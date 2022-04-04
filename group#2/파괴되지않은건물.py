e8599603bb8c31f25e80669970266cf970c36fc4
istywork <88966983+istywork@users.noreply.github.com>
2022-04-04 16:42:20 +0900

    Update 파괴되지않은건물.py

--- group#2/파괴되지않은건물.py
+++ group#2/파괴되지않은건물.py
@@ -1,28 +1,23 @@
-
-
 def solution(board, skill):
     answer = 0
-    tmp = [[0]*(len(board[0]) + 1) for _ in range(len(board) + 1)]
-
+    tmp = [[0] * (len(board[0]) + 1) for _ in range(len(board) + 1)]
     for type, r1, c1, r2, c2, degree in skill:
         tmp[r1][c1] += degree if type == 2 else -degree
-        tmp[r1][c2+1] += -degree if type == 2 else degree
-        tmp[r2+1][c1] += -degree if type == 2 else degree
-        tmp[r2+1][c2+1] += degree if type == 2 else -degree
+        tmp[r1][c2 + 1] += -degree if type == 2 else degree
+        tmp[r2 + 1][c1] += -degree if type == 2 else degree
+        tmp[r2 + 1][c2 + 1] += degree if type == 2 else -degree
 
-        for i in range(len(tmp) - 1):
-            for j in range(len(tmp[0]) - 1):
-                tmp[i][j+1] += tmp[i][j]
-        
+    for i in range(len(tmp) - 1):
         for j in range(len(tmp[0]) - 1):
-            for i in range(len(tmp) - 1):
-                tmp[i+1][j] += tmp[i][j]
-        
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                board[i][j] += tmp[i][j]
-
-                if board[i][j] > 0:
-                    answer += 1
-
+            tmp[i][j + 1] += tmp[i][j]
+ 
+    for j in range(len(tmp[0]) - 1):
+        for i in range(len(tmp) - 1):
+            tmp[i + 1][j] += tmp[i][j]
+ 
+    for i in range(len(board)):
+        for j in range(len(board[i])):
+            board[i][j] += tmp[i][j]
+            if board[i][j] > 0: answer += 1
+ 
     return answer
