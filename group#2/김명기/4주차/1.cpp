20c1a5822eb2bebd3dc5a380a711e621bd50e636
riroan <riroan@naver.com>
2022-04-12 17:31:21 +0900

    4주차

---
+++ group#2/김명기/4주차/1.cpp
@@ -0,0 +1,35 @@
+// 세 수 정렬
+
+#include<bits/stdc++.h>
+#define wow std::cin >>
+#define vv >>
+#define ww << " " <<
+#define love <
+#define hate >
+#define happy(a,b) std::swap(a,b);
+#define sad std::cout <<
+#define really if
+#define oOwOo return
+#define blob << std::endl
+#define nope else
+#define go main(){
+#define oOvOo 0
+#define owo int
+#define aww ;
+#define finish }
+
+owo go
+    owo o3o, ovo, o2o aww
+    wow o3o vv ovo vv o2o aww
+    
+    really(o3o hate ovo)
+        happy(o3o, ovo)
+    really(o2o love o3o)
+        happy(o2o, o3o)
+    really(ovo hate o2o)
+        happy(ovo, o2o)
+    
+    sad o3o ww ovo ww o2o blob aww
+    
+    oOwOo oOvOo aww
+finish
